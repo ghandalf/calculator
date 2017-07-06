a23f66af6f538c46b35337fcdbb908f5f14810ea
fouelletfresche <francis.ouellet@freschesolutions.com>
2017-07-06 18:45:12 -0400

    Functionnalities added to the project

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into Item(id, sku, name, description, price)
+values (233, 233233233, 'SamsungV233', 'Samsung 233 Smart TV', 233.00);
+insert into Item(id, sku, name, description, price)
+values (244, 244244244, 'SamsungV244', 'Samsung 244 Smart TV', 244.00);
+insert into Item(id, sku, name, description, price)
+values (255, 255255255, 'SamsungV255', 'Samsung 255 Smart TV', 255.00);
+insert into Item(id, sku, name, description, price)
+values (266, 266266266, 'SamsungV266', 'Samsung 266 Smart TV', 266.00);
